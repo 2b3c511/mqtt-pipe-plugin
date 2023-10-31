f03f2e747e39f597a0831ead1bc74010974598af
2b3c511 <1916417519@qq.com>
2023-10-31 12:08:14 +0800

    first commit

--- src/main/java/org/apache/iotdb/mqttpipeplugin/service/MQTTPipeConnector.java
+++ src/main/java/org/apache/iotdb/mqttpipeplugin/service/MQTTPipeConnector.java
@@ -1,11 +1,5 @@
 package org.apache.iotdb.mqttpipeplugin.service;
 
-import com.alibaba.fastjson2.JSON;
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.iotdb.mqttpipeplugin.event.MQTTEvent;
-import org.apache.iotdb.mqttpipeplugin.util.MQTTClientUtils;
 import org.apache.iotdb.pipe.api.PipeConnector;
 import org.apache.iotdb.pipe.api.customizer.configuration.PipeConnectorRuntimeConfiguration;
 import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameterValidator;
@@ -20,19 +14,13 @@ public class MQTTPipeConnector implements PipeConnector {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MQTTPipeConnector.class);
 
-    private final String SEND_MQTT_URI = "connector.send_mqtt_uri";
-
-    private String sendMqttUri = "";
-
     @Override
     public void validate(PipeParameterValidator validator) throws Exception {
-        validator.validateRequiredAttribute(SEND_MQTT_URI);
     }
 
     @Override
     public void customize(PipeParameters parameters, PipeConnectorRuntimeConfiguration pipeConnectorRuntimeConfiguration) throws Exception {
-        sendMqttUri=parameters.getString(SEND_MQTT_URI);
-        LOGGER.info("MQTTPipeConnector configuration: {}", parameters);
+
     }
 
     @Override
@@ -56,16 +44,6 @@ public class MQTTPipeConnector implements PipeConnector {
 
     @Override
     public void transfer(Event event) {
-        if (event instanceof MQTTEvent) {
-            try {
-                MQTTEvent mqttEvent = (MQTTEvent) event;
-                String alarmEventStr = JSON.toJSONString(mqttEvent) ;
-                MQTTClientUtils.sendMQTTRequest(mqttEvent.getDevice(),sendMqttUri, alarmEventStr);
-            } catch (Exception e) {
-                LOGGER.error("alarmPipeConnector transfer {} error,", sendMqttUri,e);
-                e.printStackTrace();
-            }
-        }
     }
 
     @Override
--- src/main/java/org/apache/iotdb/mqttpipeplugin/service/MQTTPipeProcesser.java
+++ src/main/java/org/apache/iotdb/mqttpipeplugin/service/MQTTPipeProcesser.java
@@ -19,11 +19,7 @@
 
 package org.apache.iotdb.mqttpipeplugin.service;
 
-import org.apache.iotdb.db.pipe.event.common.tablet.PipeInsertNodeTabletInsertionEvent;
-import org.apache.iotdb.db.pipe.event.common.tablet.PipeRawTabletInsertionEvent;
-import org.apache.iotdb.mqttpipeplugin.event.MQTTEvent;
 import org.apache.iotdb.pipe.api.PipeProcessor;
-import org.apache.iotdb.pipe.api.access.Row;
 import org.apache.iotdb.pipe.api.collector.EventCollector;
 import org.apache.iotdb.pipe.api.customizer.configuration.PipeProcessorRuntimeConfiguration;
 import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameterValidator;
@@ -31,16 +27,8 @@ import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameters;
 import org.apache.iotdb.pipe.api.event.Event;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TabletInsertionEvent;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TsFileInsertionEvent;
-import org.apache.iotdb.pipe.api.type.Type;
-import org.apache.iotdb.tsfile.write.record.Tablet;
-import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MQTTPipeProcesser implements PipeProcessor {
 
@@ -59,83 +47,10 @@ public class MQTTPipeProcesser implements PipeProcessor {
   @Override
   public void process(TabletInsertionEvent tabletInsertionEvent, EventCollector eventCollector)
       throws Exception {
-    eventProcess(tabletInsertionEvent, eventCollector);
   }
-
-  private void eventProcess(
-      TabletInsertionEvent tabletInsertionEvent, EventCollector eventCollector) {
-    try {
-      if (tabletInsertionEvent instanceof PipeInsertNodeTabletInsertionEvent) {
-        Tablet tablet = ((PipeInsertNodeTabletInsertionEvent) tabletInsertionEvent).convertToTablet();
-        List<String> measurement = new ArrayList<>();
-        List<String> values = new ArrayList<>();
-        tablet.getSchemas().stream().forEach(t -> measurement.add(t.getMeasurementId()));
-        tabletInsertionEvent.processRowByRow(
-                (row, rowCollector) -> {
-                  try {
-                    for (int i = 0; i < measurement.size(); i++) {
-                      values.add(String.valueOf(getValueBasedOnType(row, i)));
-                    }
-                    Event mqttEvent = new MQTTEvent(row.getDeviceId(), row.getTime(), measurement, values);
-                    eventCollector.collect(mqttEvent);
-                  } catch (Exception e) {
-                    LOGGER.error("Error occurred while processing row.", e);
-                  }
-                });
-      } else if (tabletInsertionEvent instanceof PipeRawTabletInsertionEvent) {
-        Tablet tablet = ((PipeRawTabletInsertionEvent) tabletInsertionEvent).convertToTablet();
-        List<String> measurement = new ArrayList<>();
-        List<String> values = new ArrayList<>();
-        tablet.getSchemas().stream().forEach(t -> measurement.add(t.getMeasurementId()));
-        tabletInsertionEvent.processRowByRow(
-                (row, rowCollector) -> {
-                  try {
-                    for (int i = 0; i < measurement.size(); i++) {
-                      values.add(String.valueOf(getValueBasedOnType(row, i)));
-                    }
-                    Event mqttEvent = new MQTTEvent(row.getDeviceId(), row.getTime(), measurement, values);
-                    eventCollector.collect(mqttEvent);
-                  } catch (Exception e) {
-                    LOGGER.error("Error occurred while processing row.", e);
-                  }
-                });
-      } else {
-        LOGGER.warn(
-                "IoTDBThriftSyncConnector only support "
-                        + "PipeInsertNodeTabletInsertionEvent and PipeRawTabletInsertionEvent. "
-                        + "Ignore {}.",
-                tabletInsertionEvent);
-      }
-    }catch (Exception e){
-      LOGGER.error("eventProcess Error:",e);
-    }
-  }
-
-  private double getValueBasedOnType(Row row, int i) throws IOException {
-    Type type = row.getDataType(i);
-    switch (type) {
-      case INT32:
-        return row.getInt(i);
-      case INT64:
-        return row.getLong(i);
-      case FLOAT:
-        return row.getFloat(i);
-      case DOUBLE:
-        return row.getDouble(i);
-      case BOOLEAN:
-        return row.getBoolean(i) ? 1 : 0;
-      default:
-        LOGGER.error("Unsupported data type: {}", type);
-        return 0;
-    }
-  }
-
   @Override
   public void process(TsFileInsertionEvent tsFileInsertionEvent, EventCollector eventCollector)
       throws Exception {
-    tsFileInsertionEvent
-        .toTabletInsertionEvents()
-        .forEach(event -> eventProcess(event, eventCollector));
   }
 
   @Override
